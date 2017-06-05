80c9a1782afcbfbb21d9872dce29963b3b1a9166
Daniel Alejandro Orozco <dajandro95@gmail.com>
2017-06-04 21:59:32 -0600

    Create cts.js

---
+++ cts.js
@@ -0,0 +1,12 @@
+// Constants
+
+var constants = function(){
+    this.empty = 0;
+    this.black = 1;
+    this.white = 2;
+    this.max_depth = 7;
+    this.board_size = 8;
+    this.inf = 99999999999999999;
+}
+
+module.exports = constants;
